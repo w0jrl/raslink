08efdb75c1e8f6d96be028d7e5f8c9df3cf66a30
w0jrl <admin@jlappliedtechnologies.com>
2016-05-02 21:17:39 -0600

    Fix scripts and uricheck execution
     * Update scripts to prevent printing not found messages
     * Fix uricheck soft links

--- AllStar-build/debian/update-stage2.sh
+++ AllStar-build/debian/update-stage2.sh
@@ -5,7 +5,7 @@
 # Script Start
 echo "Running update, stage two."
 echo "This will take a while."
-killall rc.local
+killall rc.local &>/dev/null
 (systemctl stop asterisk;systemctl stop dahdi)
 sleep 1
 echo "Your node can not be used durring this process. It has been disabled."
@@ -56,13 +56,11 @@ echo "Updating start up scripts..."
 cp /usr/src/utils/AllStar-build/configs/modules.conf /etc/asterisk/modules.conf
 (cp /usr/src/utils/AllStar-build/common/rc.allstar /usr/local/bin/rc.allstar;chmod +x /usr/local/bin/rc.allstar)
 (cp /usr/src/utils/AllStar-build/common/rc.updatenodelist /usr/local/bin/rc.updatenodelist;chmod +x /usr/local/bin/rc.updatenodelist)
-ln -fs /usr/src/utils/AllStar-build/common/asterisk-restart.sh /usr/bin/asterisk-restart
-chmod +x /usr/bin/asterisk-restart
 chmod +x /usr/src/utils/AllStar-build/common/asterisk-restart.sh
+ln -fs /usr/src/utils/AllStar-build/common/asterisk-restart.sh /usr/bin/asterisk-restart
 ln -fs /tmp/rpt_extnodes /var/lib/asterisk/rpt_extnodes
-ln -fs /usr/src/utils/AllStar-build/common/uricheck.sh /usr/bin/uricheck
-chmod +x /usr/bin/uricheck
 chmod +x /usr/src/utils/AllStar-build/common/uricheck.sh
+ln -fs /usr/src/utils/AllStar-build/common/uricheck.sh /usr/bin/uricheck
 echo "Done"
 sleep 1
 echo "Resetting compiler flags..."
