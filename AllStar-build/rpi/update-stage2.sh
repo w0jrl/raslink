bede9057c9c5aface81eff6e8314e4169191ce7c
w0jrl <admin@jlappliedtechnologies.com>
2016-05-14 01:32:44 -0600

    Change compiler flag reset method

--- AllStar-build/rpi/update-stage2.sh
+++ AllStar-build/rpi/update-stage2.sh
@@ -52,7 +52,7 @@ echo "Done"
 sleep 1
 echo "Resetting compiler flags..."
 cd /usr/src/utils
-git reset --hard HEAD
+git checkout -f
 echo "Done"
 sleep 1
 echo "Updating system boot configuration..."
