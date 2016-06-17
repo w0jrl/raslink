1e865cc8e54a80e222eb59f2e604915fe5491e59
w0jrl <admin@jlappliedtechnologies.com>
2016-06-16 21:09:28 -0600

    Remove redundant adding of Asterisk version durring stage-1 of system-update
    	* Affects both platforms
    This is handled by the update-asterisk script already.

--- AllStar-build/rpi/update-stage1.sh
+++ AllStar-build/rpi/update-stage1.sh
@@ -10,18 +10,15 @@ sleep 1
 echo "Updating source files for All Star..."
 cd /usr/src/utils
 git pull
-cd /usr/src/utils/astsrc
-svnversion >asterisk/.version
+sleep 1
 echo "Done"
 # update the system
 echo "Updating system software..."
-sleep 1
 (apt-get update;apt-get dist-upgrade -y)
 sleep 1
 echo "Done"
 # clean the package database
 echo "Cleaning up unneeded software..."
-sleep 1
 (apt-get autoremove --purge -y;apt-get clean;apt-get autoclean)
 sleep 1
 echo "Done"
@@ -31,7 +28,6 @@ mv .bashrc .bashrc.orig
 cat .bashrc.orig > .bashrc
 echo "/usr/src/utils/AllStar-build/rpi/update-stage2.sh" >> .bashrc
 echo "Rebooting to finish install."
-sleep 1
 sync
 sudo reboot
 
