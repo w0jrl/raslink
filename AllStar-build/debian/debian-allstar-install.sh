30e42e5dd9eb23f2af7dc2e0398216e7346dafef
w0jrl <admin@jlappliedtechnologies.com>
2016-07-27 09:22:28 -0600

    Update Debian install script to use git clone instead of wget

--- AllStar-build/debian/debian-allstar-install.sh
+++ AllStar-build/debian/debian-allstar-install.sh
@@ -3,13 +3,13 @@ echo "Welcome to the AllStar Debian installer."
 echo "This script will install AllStar on your Debian server."
 echo "If you don't want to install AllStar, press CTRL+c now to abort."
 sleep 5
-echo " Installing Unzip and zip..."
-apt-get install unzip zip -y
+echo " Installing Git..."
+apt-get install git -y
 echo "Done"
 echo "downloading and unpacking source files."
 echo "This might take a while."
 cd /usr/src
-(wget https://github.com/w0jrl/AllStar-raspbian/archive/master.zip;unzip master.zip;mv AllStar-raspbian-master utils;rm -rf master.zip)
+git clone https://github.com/w0jrl/AllStar-raspbian.git utils
 echo "Done"
 sleep 1
 echo "Updating the system."
@@ -25,7 +25,7 @@ chmod +x /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
 /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
 echo "Done"
 sleep 1
-echo "Installing packages."
+echo "Installing packages needed for stage two..."
 chmod +x /usr/src/utils/AllStar-build/common/required-tools.sh
 /usr/src/utils/AllStar-build/common/required-tools.sh
 echo "Done"
