5dddede75b7f7669b463f53c491e7b76e70b1950
w0jrl <admin@jlappliedtechnologies.com>
2017-02-25 02:51:13 -0700

    Update required-tools.sh and rc.updatenodelist

--- AllStar-build/common/required-tools.sh
+++ AllStar-build/common/required-tools.sh
@@ -8,30 +8,13 @@
 # You have been warned!
 
 # Script Start
-apt-get install ntpdate\
-g++\
-make\
-build-essential automake\
-git\
-sudo ca-certificates\
-libusb-dev\
-libnewt-dev\
-libeditline0\
-libncurses5-dev\
-bison\
-libssl-dev\
-libasound2-dev\
-libcurl4-gnutls-dev\
-php5-cli\
-libiksemel-dev\
-libvorbis-dev\
-curl\
-sox\
-usbutils\
-libsqlite3-dev sqlite3\
-screen\
-fxload bc alsa-utils dnsutils\
-php5-curl libtool autoconf libical-dev libspandsp-dev libneon27-dev libxml2-dev pkg-config unixodbc-dev uuid uuid-dev libsrtp0-dev -y
+apt-get install -y ntpdate g++ make build-essential automake git\
+  sudo ca-certificates libusb-dev libnewt-dev libeditline0\
+  libncurses5-dev bison libssl-dev libasound2-dev\
+  libcurl4-gnutls-dev php5-cli libiksemel-dev libvorbis-dev\
+  curl sox libsox-fmt-all usbutils libsqlite3-dev sqlite3 screen rsync\
+  fxload bc alsa-utils dnsutils php5-curl libtool autoconf\
+  libical-dev libspandsp-dev libneon27-dev libxml2-dev pkg-config unixodbc-dev uuid uuid-dev libsrtp0-dev
 apt-get build-dep dahdi -y
 # put the linker where Asterisk expects it
 ln -fs /bin/ln /usr/bin
