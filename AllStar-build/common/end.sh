0964491548581bf473515261e61591bb959f8764
w0jrl <admin@jlappliedtechnologies.com>
2017-04-29 18:13:46 -0600

    Add end command
     Used to disconnect links from AllStar
    For help, run 'end.sh -h'.

---
+++ AllStar-build/common/end.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# end.sh - Used to disconnect links from AllStar
+
+# Script Start
+/usr/sbin/asterisk -rx "rpt localnodes" > /tmp/localnodes
+remove()
+{
+    rm /tmp/localnodes &>/dev/null
+}
+help()
+{
+   cat << _HELP_
+Usage:
+End can be used in two ways.
+If you type only the node to control,
+End assumes that you want to drop all connected nodes from that node.
+The node to control must be on your AllStar server.
+example:
+end 12345
+If you type the node to control plus another node number,
+End will drop that node.
+example:
+end 12345 67890
+You must enter at least the node  to control.
+_HELP_
+}
+if [[ "$1" = "" ]] || [[ "$1" = "0" ]]; then
+  remove
+  help
+  exit 2
+  elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]; then
+    remove
+    help 
+    exit 1
+  elif [ "$1" != "$( grep $1 /tmp/localnodes )" ]; then
+    echo "You cannot control $1.
+$1 doesn't exist on this server."
+    remove
+    exit 2
+elif [[ "$2" = "0" ]] || [[ "$2" = "$1" ]]; then
+    echo "You cannot disconnect a node from itself.
+Try again."
+    remove
+    exit 2
+  elif [ "$2" != "" ]; then
+    /usr/sbin/asterisk -rx "rpt cmd $1 ilink 1 $2"
+    echo "$2 disconnected from $1"
+else
+  /usr/sbin/asterisk -rx "rpt cmd $1 ilink 6 $1"
+  echo "All links disconnected from $1"
+fi
+remove
+exit 0
--- AllStar-build/rpi/make-links.sh
+++ AllStar-build/rpi/make-links.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# make-links.sh - Create the system-update, wifi-setup, asterisk-restart, uricheck, version, and node list links.
+# make-links.sh - Create system soft links for AllStar
 
 # Script Start
 chmod +x /usr/src/utils/AllStar-build/rpi/update-stage1.sh
@@ -13,5 +13,7 @@ chmod +x /usr/src/utils/AllStar-build/common/uricheck.sh
 ln -fs /usr/src/utils/AllStar-build/common/uricheck.sh /usr/bin/uricheck
 chmod +x /usr/src/utils/AllStar-build/common/version.sh
 ln -fs /usr/src/utils/AllStar-build/common/version.sh /usr/bin/version
+chmod +x /usr/src/utils/AllStar-build/common/end.sh
+ln -fs /usr/src/utils/AllStar-build/common/end.sh /usr/bin/end
 ln -fs /etc/network/if-up.d/ntpdate /etc/cron.hourly/ntpdate
 exit 0
