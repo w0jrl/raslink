c309a7fabdb526f1a3248b32f3e7b2251a4d1d04
w0jrl <admin@jlappliedtechnologies.com>
2017-05-01 02:17:12 -0600

    Clean Libpri before building

--- AllStar-build/common/update-libpri.sh
+++ AllStar-build/common/update-libpri.sh
@@ -7,6 +7,6 @@ cd /usr/src/utils/astsrc/libpri/
 # Patch libpri for use with AllStar
 patch </usr/src/utils/AllStar-build/patches/patch-libpri-makefile
 # Build and install libpri
-(make all;make install)
+(make clean;make all;make install)
 echo "Done"
 exit 0
