98a34efce33b69ea61272a7c0be3f95da1205d45
w0jrl <admin@jlappliedtechnologies.com>
2017-05-01 01:35:13 -0600

    Clean Asterisk before building

--- AllStar-build/common/update-asterisk.sh
+++ AllStar-build/common/update-asterisk.sh
@@ -7,7 +7,7 @@ cd /usr/src/utils/astsrc/asterisk/
 # Put git commit number where asterisk makefile expects it
 git describe --always > .version
 # Configure the build process
-./configure
+(make clean;./configure)
 # Build and install Asterisk
 (make all;make install)
 # Fix comment in rpt.conf
