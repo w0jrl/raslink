abbb6d5e0b5d584a15863dc71ef33e3e9681dcb9
w0jrl <admin@jlappliedtechnologies.com>
2017-07-28 00:53:46 -0600

    Add irqbalance as required package
     Balance cpu interrupts to make timing better.

--- AllStar-build/common/required-tools.sh
+++ AllStar-build/common/required-tools.sh
@@ -10,7 +10,7 @@ apt-get install -y ntpdate g++ make build-essential automake git\
   curl sox libsox-fmt-all usbutils libsqlite3-dev sqlite3 screen\
   fxload bc alsa-utils dnsutils php5-curl libtool autoconf gawk\
   libical-dev libspandsp-dev libneon27-dev libxml2-dev pkg-config unixodbc-dev uuid uuid-dev libsrtp0-dev dkms\
-  libatomic-ops-dev libatomic1
+  libatomic-ops-dev libatomic1 irqbalance
 apt-get build-dep dahdi -y
 if [[ $distro = "Raspbian" ]]; then
   apt-get install i2c-tools libi2c-dev
