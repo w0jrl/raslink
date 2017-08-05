99384f5d431108bbc611a6a98d96996aef0298f2
w0jrl <admin@jlappliedtechnologies.com>
2017-08-05 01:17:30 -0600

    Remove low pass and high pass filters from usbradio
     They weren't being used.

--- AllStar-build/common/update-asterisk.sh
+++ AllStar-build/common/update-asterisk.sh
@@ -22,6 +22,8 @@ cd /usr/src/utils/astsrc/asterisk/
 # Put git commit number where asterisk makefile expects it
 git describe --always > .version
 # Configure the build process
+# Remove modules from previous build
+rm -f /usr/lib/asterisk/modules/* &>/dev/null
 distro=$(lsb_release -is)
 if [[ $distro = "Raspbian" ]]; then
   sed -i '/PROC\=/c\PROC\=arm' ./makeopts.in
@@ -31,13 +33,12 @@ fi
 (make all;make install)
 # Fix comment in rpt.conf
 sed -i 's/Say phonetic call sign/Say call sign/' /etc/asterisk/rpt* | sed -i 's/say phonetic call sign/Say call sign/' /etc/asterisk/rpt*
-# Remove unneeded filter comments in usbradio.conf
-sed -i '/Audio to internet/d' /etc/asterisk/usbradio*
-sed -i '/Audio from internet/d' /etc/asterisk/usbradio*
 # Load res_crypto module
 sed -i '/res_crypto.so/c\load \=> res_crypto.so ;   Cryptographic Digital Signatures                  ' /etc/asterisk/modules.conf
 # Load app_sendtext module
 sed -i '/app_sendtext.so/c\load \=> app_sendtext.so ;   Send Text Applications                            ' /etc/asterisk/modules.conf
+# Remove low pass and high pass filter configuration from usbradio
+sed -i -e '/rxlpf \= 0/,+19d' /etc/asterisk/usbradio.conf
 # Increase jbmaxsize in usbradio
 sed -i 's/jbmaxsize \= 200/jbmaxsize \= 500/' /etc/asterisk/usbradio*
 echo "Done."
