ef598d800d9201aca96656f5a31b620e5283459e
w0jrl <admin@jlappliedtechnologies.com>
2017-09-25 05:50:21 -0600

    Update queue size in usbradio driver
    A queue size of 10 was causing audio to stutter

--- AllStar-build/common/patch-asterisk.sh
+++ AllStar-build/common/patch-asterisk.sh
@@ -28,8 +28,8 @@ cp ../extras/mdc1200/*.h ./apps
 sed -i 's/\/\* #include "mdc_decode.c" \*\//#include "mdc_decode.c"/' ./apps/app_rpt.c
 sed -i 's/\/\* #include "mdc_encode.c" \*\//#include "mdc_encode.c"/' ./apps/app_rpt.c
 # Update app_rpt version information
-sed -i '/ \*  version/c\ \*  version 17.09 09\/01\/2017' ./apps/app_rpt.c
-sed -i '/static  char \*tdesc \= "Radio Repeater \/ Remote Base  version/c\static  char \*tdesc \= "Radio Repeater \/ Remote Base  version 17.09 09\/01\/2017";' ./apps/app_rpt.c
+sed -i '/ \*  version/c\ \*  version 17.10 10\/01\/2017' ./apps/app_rpt.c
+sed -i '/static  char \*tdesc \= "Radio Repeater \/ Remote Base  version/c\static  char \*tdesc \= "Radio Repeater \/ Remote Base  version 17.10 10\/01\/2017";' ./apps/app_rpt.c
 # Change TX enabled message
 sed -i 's/"RPTENA"/"TXENA"/' ./apps/app_rpt.c
 # Make EchoLink call signs use normal characters
@@ -37,8 +37,8 @@ sed -i 's/return(sayphoneticstr/return\(saycharstr/' ./apps/app_rpt.c
 sed -i 's/res \= sayphoneticstr/res \= saycharstr/' ./apps/app_rpt.c
 # Prevent reading false DTMF digits
 sed -i 's/26.0 : 42.0/32.0 : 42.0/' ./main/dsp.c
-# Increase queue size in usbradio driver
-sed -i 's/define	QUEUE_SIZE	2/define	QUEUE_SIZE	10/' ./channels/chan_usbradio.c
+# Set queue size in usbradio driver
+sed -i 's/define	QUEUE_SIZE	10/define	QUEUE_SIZE	3/' ./channels/chan_usbradio.c
 # Make sure asterisk uses pthread
 sed -i 's/ASTCFLAGS+=-Wno-unused-result/ASTCFLAGS\+\=-pthread -Wno-unused-result/' ./Makefile
 echo "Done"
