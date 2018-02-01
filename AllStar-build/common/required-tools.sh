182ab012cf6f369f8e1568185b8bd21c2378c250
w0jrl <admin@jlappliedtechnologies.com>
2018-01-31 19:40:52 -0700

    Update package requirements to work with Debian stretch

--- AllStar-build/common/required-tools.sh
+++ AllStar-build/common/required-tools.sh
@@ -18,15 +18,21 @@
 
 # Script Start
 distro=$(lsb_release -is)
+release=$(lsb_release -cs)
 apt-get -qq install -y ntpdate g++-4.8  g++ make build-essential automake git lsb-release\
   sudo ca-certificates libusb-dev libnewt-dev libeditline-dev libedit-dev\
   libncurses5-dev libncursesw5-dev bison libssl-dev libasound2-dev\
-  libcurl4-openssl-dev php5-cli libiksemel-dev libvorbis-dev\
+  libcurl4-openssl-dev libiksemel-dev libvorbis-dev\
   curl sox libsox-fmt-all usbutils libsqlite3-dev sqlite3 screen\
-  fxload bc alsa-utils dnsutils php5-curl libtool autoconf gawk\
+  fxload bc alsa-utils dnsutils libtool autoconf gawk\
   libical-dev libspandsp-dev libneon27-dev libxml2-dev pkg-config libspeex-dev unixodbc unixodbc-dev uuid uuid-dev libsrtp0-dev dkms\
   libatomic-ops-dev libatomic1 irqbalance libresample-dev libgmime-2.6-dev libpt-dev
 apt-get -qq build-dep dahdi -y
+if [[ $release = "stretch" ]]; then
+  apt-get -qq install -y php-cli php-curl
+else
+  apt-get -qq install -y php5-cli php5-curl
+fi
 if [[ $distro = "Raspbian" ]]; then
   apt-get -qq install -y i2c-tools libi2c-dev raspberrypi-kernel raspberrypi-kernel-headers fake-hwclock
 else
