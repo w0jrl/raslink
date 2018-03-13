1f7c0e945d0c60049618dcda67c0e9c6310c8308
w0jrl <admin@jlappliedtechnologies.com>
2018-03-13 01:33:16 -0600

    Update libneon27 requirement for stretch

--- AllStar-build/common/required-tools.sh
+++ AllStar-build/common/required-tools.sh
@@ -26,13 +26,13 @@ apt-get -qq install -y ntpdate g++-4.8  g++ make build-essential automake git\
   libcurl4-openssl-dev libiksemel-dev libvorbis-dev\
   curl sox libsox-fmt-all usbutils libsqlite3-dev sqlite3 screen\
   fxload bc alsa-utils dnsutils libtool autoconf gawk\
-  libical-dev libspandsp-dev libneon27-dev libxml2-dev pkg-config libspeex-dev unixodbc unixodbc-dev uuid uuid-dev libsrtp0-dev dkms\
+  libical-dev libspandsp-dev libxml2-dev pkg-config libspeex-dev unixodbc unixodbc-dev uuid uuid-dev libsrtp0-dev dkms\
   libatomic-ops-dev libatomic1 irqbalance libresample-dev libgmime-2.6-dev libpt-dev
 apt-get -qq build-dep dahdi -y
 if [[ $release = "stretch" ]]; then
-  apt-get -qq install -y php-cli php-curl libssl1.0-dev
+  apt-get -qq install -y php-cli php-curl libssl1.0-dev libneon27[gnutls-dev
 else
-  apt-get -qq install -y php5-cli php5-curl libssl-dev
+  apt-get -qq install -y php5-cli php5-curl libssl-dev libneon27-dev
 fi
 if [[ $distro = "Raspbian" ]]; then
   apt-get -qq install -y i2c-tools libi2c-dev raspberrypi-kernel raspberrypi-kernel-headers fake-hwclock
