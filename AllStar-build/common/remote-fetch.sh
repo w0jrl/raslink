cf707d10b2d56ac5cb0f5f96f27a58825c26e259
w0jrl <admin@jlappliedtechnologies.com>
2018-06-13 13:32:13 -0600

    Move RasLink to Gitlab

--- AllStar-build/common/remote-fetch.sh
+++ AllStar-build/common/remote-fetch.sh
@@ -19,9 +19,9 @@
 # Script Start
 cd /usr/src/utils
 echo "Checking repository URL..."
-if [ "$(git config --get remote.origin.url | grep -ic "AllStar-raspbian")" == "1" ]; then
+if [ "$(git config --get remote.origin.url | grep -ic "github")" == "1" ]; then
   echo "Updating repository URL and data..."
-  git remote set-url origin https://github.com/w0jrl/raslink.git
+  git remote set-url origin https://gitlab.com/w0jrl/raslink.git
   git pull &>/dev/null
   echo "Done"
 else
