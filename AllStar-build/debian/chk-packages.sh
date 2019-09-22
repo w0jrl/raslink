96ef36ba9fe3db8e8d97fd155bb76b9c8b43d75a
w0jrl <admin@jlappliedtechnologies.com>
2019-09-22 09:07:18 -0600

    Remove useless motd file
     Screen reader users were getting spammed on log in by a message
    that sighted users couldn't see.

--- AllStar-build/debian/chk-packages.sh
+++ AllStar-build/debian/chk-packages.sh
@@ -21,11 +21,14 @@ echo "Removing unneeded packages and data"
 subversion=/usr/bin/svn
 echo "Checking Subversion..."
 if [ -e "$subversion" ]; then
-  echo "Removing Subversion; No longer needed for AllStar."
-  apt-get -qq autoremove --purge -y subversion
-  rm -rf /root/.subversion &>/dev/null
+    echo "Removing Subversion; No longer needed for AllStar."
+    apt-get -qq autoremove --purge -y subversion
+    rm -rf /root/.subversion &>/dev/null
 else
-  echo "Subversion isn't installed; Skipping."
+    echo "Subversion isn't installed; Skipping."
+fi
+if [ -f /etc/motd ]; then
+    rm -rf /etc/motd*
 fi
 echo -e "Done\n"
 echo "Checking and installing required packages..."
@@ -40,21 +43,21 @@ echo -e "Done\n"
 echo "checking Logrotate status..."
 fstab=$(grep -ic "/var/log tmpfs defaults,noatime,nosuid,mode=0755,size=64m 0 0" /etc/fstab )
 if [ "$fstab" == "1" ]; then
-  echo "Old fstab log setting detected; Removing."
-  sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=64m 0 0/d' /etc/fstab
-  echo "Done"
+    echo "Old fstab log setting detected; Removing."
+    sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=64m 0 0/d' /etc/fstab
+    echo "Done"
 fi
 if [[ -e /etc/logrotate.d/asterisk ]] && [[ "$(grep -ic "compress" /etc/logrotate.d/asterisk)" == "1" ]]; then
-  echo -e "Logrotate parameters are already up to date; Skipping.\n"
+    echo -e "Logrotate parameters are already up to date; Skipping.\n"
 else
-  echo "updating Logrotate parameters..."
-  sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=100m 0 0/d' /etc/fstab
-  if [ -e /etc/logrotate.d/asterisk ]; then
-    rm -rf /etc/logrotate.d/asterisk
-  fi
-  chmod +x /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
-  /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
-  echo "Logs will be rotated once a month."
-  echo -e "Done\n"
+    echo "updating Logrotate parameters..."
+    sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=100m 0 0/d' /etc/fstab
+    if [ -e /etc/logrotate.d/asterisk ]; then
+        rm -rf /etc/logrotate.d/asterisk
+    fi
+    chmod +x /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
+    /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
+    echo "Logs will be rotated once a month."
+    echo -e "Done\n"
 fi
 exit 0
--- AllStar-build/rpi/chk-packages.sh
+++ AllStar-build/rpi/chk-packages.sh
@@ -20,50 +20,53 @@
 echo "Removing unneeded packages and data"
 echo "Looking for Wolfram Engine..."
 if [ -e /usr/bin/wolfram ]; then
-  echo "Uninstalling Wolfram Engine; Not needed for AllStar."
-  apt-get -qq purge -y wolfram-engine &>/dev/null
-  echo "Cleaning the database"
-  (apt-get autoremove --purge -y;apt-get clean;apt-get autoclean) &>/dev/null
-  echo "Removing Wolfram Engine data"
-  rm -rf /opt/Wolfram &>/dev/null
+    echo "Uninstalling Wolfram Engine; Not needed for AllStar."
+    apt-get -qq purge -y wolfram-engine &>/dev/null
+    echo "Cleaning the database"
+    (apt-get autoremove --purge -y;apt-get clean;apt-get autoclean) &>/dev/null
+    echo "Removing Wolfram Engine data"
+    rm -rf /opt/Wolfram &>/dev/null
 else
-  echo "Wolfram Engine isn't installed; Skipping."
+    echo "Wolfram Engine isn't installed; Skipping."
 fi
 echo "Looking for Minecraft..."
 if [ -e /usr/bin/minecraft-pi ]; then
-  echo "Uninstalling Minecraft; Not needed for AllStar."
-  apt-get -qq purge -y minecraft-pi &>/dev/null
-  echo "Cleaning the database"
-  (apt-get autoremove --purge -y;apt-get clean;apt-get autoclean) &>/dev/null
+    echo "Uninstalling Minecraft; Not needed for AllStar."
+    apt-get -qq purge -y minecraft-pi &>/dev/null
+    echo "Cleaning the database"
+    (apt-get autoremove --purge -y;apt-get clean;apt-get autoclean) &>/dev/null
 else
-  echo "Minecraft isn't installed; Skipping."
+    echo "Minecraft isn't installed; Skipping."
 fi
 chmod +x /usr/src/utils/AllStar-build/rpi/rm-pi.sh
 /usr/src/utils/AllStar-build/rpi/rm-pi.sh
 echo "Looking for RPI Update..."
 if [ -e /usr/bin/rpi-update ]; then
-  echo "Uninstalling RPI Update; No longer needed for AllStar."
-  apt-get purge -y rpi-update &>/dev/null
-  echo "Cleaning the database"
-  (apt-get autoremove --purge -y;apt-get clean;apt-get autoclean) &>/dev/null
+    echo "Uninstalling RPI Update; No longer needed for AllStar."
+    apt-get purge -y rpi-update &>/dev/null
+    echo "Cleaning the database"
+    (apt-get autoremove --purge -y;apt-get clean;apt-get autoclean) &>/dev/null
 else
-  echo "RPI Update isn't installed; Skipping."
+    echo "RPI Update isn't installed; Skipping."
 fi
 subversion=/usr/bin/svn
 echo "Checking Subversion..."
 if [ -e "$subversion" ]; then
-  echo "Removing Subversion; No longer needed for AllStar."
-  apt-get -qq autoremove --purge -y subversion
-  rm -rf /root/.subversion &>/dev/null
+    echo "Removing Subversion; No longer needed for AllStar."
+    apt-get -qq autoremove --purge -y subversion
+    rm -rf /root/.subversion &>/dev/null
 else
-  echo "Subversion isn't installed; Skipping."
+    echo "Subversion isn't installed; Skipping."
+fi
+if [ -f /etc/motd ]; then
+    rm -rf /etc/motd*
 fi
 echo -e "Done\n"
 echo "Checking and installing required packages..."
 sourcesList=$( grep -ic "#deb-src" /etc/apt/sources.list )
 if [ "$sourcesList" == "1" ]; then
-  sed -i 's/#deb-src/deb-src/' /etc/apt/sources.list
-  apt-get -qq update
+    sed -i 's/#deb-src/deb-src/' /etc/apt/sources.list
+    apt-get -qq update
 fi
 chmod +x /usr/src/utils/AllStar-build/common/required-tools.sh
 /usr/src/utils/AllStar-build/common/required-tools.sh
@@ -71,25 +74,25 @@ echo -e "Done\n"
 echo "checking Logrotate status..."
 fstab=$(grep -ic "/var/log tmpfs defaults,noatime,nosuid,mode=0755,size=64m 0 0" /etc/fstab )
 if [ "$fstab" == "1" ]; then
-  echo "Old fstab log setting detected; Removing."
-  sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=64m 0 0/d' /etc/fstab
-  echo "Done"
+    echo "Old fstab log setting detected; Removing."
+    sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=64m 0 0/d' /etc/fstab
+    echo "Done"
 fi
 if [[ -e /etc/logrotate.d/asterisk ]] && [[ "$(grep -ic "compress" /etc/logrotate.d/asterisk)" == "1" ]]; then
-  echo -e "Logrotate parameters are already up to date; Skipping.\n"
+    echo -e "Logrotate parameters are already up to date; Skipping.\n"
 else
-  echo "updating Logrotate parameters..."
-  sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=100m 0 0/d' /etc/fstab
-  if [ -e /etc/logrotate.d/asterisk ]; then
-    rm -rf /etc/logrotate.d/asterisk
-  fi
-  chmod +x /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
-  /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
-  echo "Logs will be rotated once a month."
-  echo -e "Done\n"
+    echo "updating Logrotate parameters..."
+    sed -i '/tmpfs \/var\/log tmpfs defaults,noatime,nosuid,mode=0755,size=100m 0 0/d' /etc/fstab
+    if [ -e /etc/logrotate.d/asterisk ]; then
+        rm -rf /etc/logrotate.d/asterisk
+    fi
+    chmod +x /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
+    /usr/src/utils/AllStar-build/common/mk-logrotate-asterisk.sh
+    echo "Logs will be rotated once a month."
+    echo -e "Done\n"
 fi
 if [ "$( grep -ic "i2c-dev" /etc/modules )" == "0" ]; then
-  echo "i2c-dev" >> /etc/modules
-  modprobe i2c-dev
+    echo "i2c-dev" >> /etc/modules
+    modprobe i2c-dev
 fi
 exit 0
