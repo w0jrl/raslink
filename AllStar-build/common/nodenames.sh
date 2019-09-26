d47a951a9cc852d5c8be088fddc1c401739977fd
w0jrl <admin@jlappliedtechnologies.com>
2019-09-26 14:02:46 -0600

    Reformat nodenames scripts

--- AllStar-build/common/nodenames.sh
+++ AllStar-build/common/nodenames.sh
@@ -33,7 +33,7 @@ enable()
 }
 help()
 {
-cat << _HELP_
+    cat << _HELP_
 Usage: nodenames [OPTION]
 [on]    Enable nodenames
 [off]   Disable nodenames
@@ -44,24 +44,24 @@ _HELP_
 status()
 {
     if [[ -e /root/.nonames ]] || [[ "$(service nodenames status | grep -ic "disabled")" = "1" ]]; then
-      echo "Nodenames disabled"
+        echo "Nodenames disabled"
     else
-      echo "Nodenames enabled"
+        echo "Nodenames enabled"
     fi
 }
 if [[ "$1" = "" ]]; then
-  help
-  exit 2
-  elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]] || [[ "$1" = "help" ]]; then
     help
-    exit 1
-  elif [ "$1" = "on" ]; then
-    enable
-    exit 0
-  elif [ "$1" = "off" ]; then
-    disable
-    exit 0
-  elif [ "$1" = "status" ]; then
-    status
+    exit 2
+    elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]] || [[ "$1" = "help" ]]; then
+        help
+        exit 1
+    elif [ "$1" = "on" ]; then
+        enable
+        exit 0
+    elif [ "$1" = "off" ]; then
+        disable
+        exit 0
+    elif [ "$1" = "status" ]; then
+        status
 fi
 exit 0
